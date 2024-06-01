dac43889891537c0967bbb59aaa8ca8f17c706d7
CarloMechatronics <carlo.torres@utec.edu.pe>
2024-06-01 17:12:50 -0500

    Changes

--- src/main/java/com/music/hackatonctysc/authentication/security/SecurityConfig.java
+++ src/main/java/com/music/hackatonctysc/authentication/security/SecurityConfig.java
@@ -1,9 +1,16 @@
 package com.music.hackatonctysc.authentication.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends{
+public class SecurityConfig {
+
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {}
 }
