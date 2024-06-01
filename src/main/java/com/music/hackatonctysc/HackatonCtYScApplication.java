33b89639686d04b4f8c5445e9b84b2ac0aa82dce
CarloMechatronics <carlo.torres@utec.edu.pe>
2024-06-01 15:24:55 -0500

    First commit

---
+++ src/main/java/com/music/hackatonctysc/HackatonCtYScApplication.java
@@ -0,0 +1,13 @@
+package com.music.hackatonctysc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonCtYScApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackatonCtYScApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/music/hackatonctysc/HackatonCtYScApplicationTests.java
@@ -0,0 +1,13 @@
+package com.music.hackatonctysc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackatonCtYScApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
